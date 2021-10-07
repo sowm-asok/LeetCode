855ae90c40e8f43994e578c8b7c17c71c66b0891
sowm-asok <35369631+sowm-asok@users.noreply.github.com>
2021-10-07 13:00:03 -0700

    Time: 2 ms (22.56%), Space: 40.4 MB (38.94%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,31 +1,23 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int temp;
+        int temp = 0;
         int left = 0;
-        int right;
-        while(left+1 < nums.length){
-            right = left+1;
-            
-            // System.out.println("left" + nums[left]);
-            // System.out.println("right" + nums[right]);
-            
-            if(nums[left] == 0 && nums[right] != 0) {
+        int right = left+1;
+        while (right < nums.length) {
+            if(nums[left] != 0){
+                left++;
+                right++;
+            }
+            else if(nums[left] == 0 && nums[right] != 0){
                 temp = nums[left];
                 nums[left] = nums[right];
                 nums[right] = temp;
+                left++;
+                right++;
             }
-            else if(nums[left] == 0 && nums[right] == 0){
-                while(right<nums.length && nums[right] == 0){
-                    right++;
-                }
-                if(right == nums.length)
-                    return;
-                temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
+            else if(nums[left] == 0 && nums[right] == 0) {
+                right++;
             }
-            
-            left+=1;
         }
     }
 }
