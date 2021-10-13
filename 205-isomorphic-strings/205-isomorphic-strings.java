41d14225f509d90384721e6a5e1f2e5cab2d1cb9
sowm-asok <35369631+sowm-asok@users.noreply.github.com>
2021-10-13 14:54:28 -0700

    Time: 12 ms (41.76%), Space: 40.8 MB (6.17%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -3,21 +3,17 @@ class Solution {
         int[] id1 = new int[s.length()];
         int[] id2 = new int[t.length()];
         int count=1;
-        if(s.length() != 0 && t.length() != 0 && s.length() == t.length()){
-            id1 = addId(s);
-            id2 = addId(t);
-            
-            for(int i=0; i < s.length(); i++) {
-                System.out.println("iteration: " + i);
-                if(id1[i] != id2[i]){
-                    System.out.println("id1" + " :" + id1[i]);
-                    System.out.println("id2" + " :" + id2[i]);
-                    return false;
-                }
-            }
-            return true;
+        if(s.length() != t.length()){
+            return false;
         }
-        return false;
+        id1 = addId(s);
+        id2 = addId(t);
+        for(int i=0; i < s.length(); i++) {
+            if(id1[i] != id2[i]){
+                return false;
+            }
+        }
+        return true;
     }
     
     public int[] addId(String s){
@@ -26,13 +22,9 @@ class Solution {
         HashMap<Character, Integer> table = new HashMap<Character, Integer>();
             for(int i=0; i < s.length(); i++) {
                 if(table.containsKey(s.charAt(i))){
-                    idOrder[i] = table.get(s.charAt(i));
-                    // System.out.println(s.charAt(i) + " :" + idOrder[i]);
-                    // table.put(s.charAt(i), table.get(s.charAt(i))+1); // why are you incrementing the id here?
-                    
+                    idOrder[i] = table.get(s.charAt(i));   
                 }
                 else {
-                    // System.out.println(s.charAt(i) + " :" + count);
                     table.put(s.charAt(i), count);
                     idOrder[i] = count;
                     count++;
